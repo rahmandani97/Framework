8d44ff34b8c12c676ff0ecd532ebbd43e6d31db9
rahmandani97 <51946581+rahmandani97@users.noreply.github.com>
2019-12-19 19:23:00 +0700

    Add files via upload

---
+++ application/views/v_footer.php
@@ -0,0 +1,26 @@
+<footer id="fh5co-footer" role="contentinfo">
+
+		<div class="container">
+			<div class="col-md-3 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>About Us</h3>
+				<p>MEDIA INFORMATIC adalah sebuah majalah online dari sekumpulan mahasiswa teknik informatika UNESA </p>
+			</div>
+
+
+			<div class="col-md-2 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Follow Us</h3>
+				<ul class="fh5co-social">
+					<li><a href="#"><i class="icon-twitter"></i></a></li>
+					<li><a href="#"><i class="icon-facebook"></i></a></li>
+					<li><a href="#"><i class="icon-google-plus"></i></a></li>
+					<li><a href="#"><i class="icon-instagram"></i></a></li>
+				</ul>
+			</div>
+
+
+			<div class="col-md-12 fh5co-copyright text-center">
+				<p>&copy; 2019 by Media Informatic</a>. Universitas Negeri Surabaya.</p>
+			</div>
+
+		</div>
+	</footer>
